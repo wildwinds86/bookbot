373c28fb61a41e9257ee5f22691e447d80d6638e
wildwinds86 <dclaydon@gmail.com>
2025-04-23 19:11:52 +0100

    updating progress

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from stats import count_words
+from stats import *
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main():
-    word_count = count_words(get_book_text("books/frankenstein.txt"))
-    print(f"{word_count} words found in the document")
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    word_count = count_words(book_text)
+    sorted_chars = sort_characters(count_characters(book_text))
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for x in sorted_chars:
+        if x["char"].isalpha():
+            print(f"{x["char"]}: {x["num"]}")
+
+    print("============= END ===============")
+    
 
 main()
--- stats.py
+++ stats.py
@@ -2,21 +2,23 @@ def count_words(book_text):
     words = book_text.split()
     return len(words)
 
+def count_characters(book_text):
+    characters = {}
+    for c in book_text.lower():
+        if c in characters:
+            characters[c] += 1
+        else: 
+            characters[c] = 1
 
+    return characters
 
+def sort_characters(characters_dict):
+    def sort_on(dict):
+        return dict["num"]
 
+    sorted_list = []
+    for char, num in characters_dict.items():
+        sorted_list.append({"char" : char, "num" : num})
 
-
-"""
-Count Characters
-Ah, the meat of bookbot: counting characters!
-
-Assignment
-Add a new function to your stats.py file that takes the text from the book as a string, and returns the number of times each character, (including symbols and spaces), appears in the string.
-Convert any character to lowercase using the .lower() method, we don't want duplicates.
-Use a dictionary of String -> Integer. The returned dictionary should look something like this:
-{'p': 6121, 'r': 20818, 'o': 25225, ...
-
-Import and call the function in main.py, and capture the result in a new variable.
-After printing the word count, print the dictionary of characters and their counts.
-"""
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
